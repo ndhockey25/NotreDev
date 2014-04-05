d45a4c2714e84c0ed97411b6382aa0ee1a78b403
EJ Layne <ej.layne@playonsports.com>
2014-04-05 16:00:56 -0700

    updated Actor class with actorCells

--- SnakesOnAPlane/src/com/notredev/snakes/Actor.java
+++ SnakesOnAPlane/src/com/notredev/snakes/Actor.java
@@ -4,7 +4,9 @@ public class Actor {
 	
 	enum ActorType {SNAKE, OBSTACLE, BULLET}
 	ActorType _type;
-	
+	java.util.LinkedList<GameBoardCell> actorCells = new java.util.LinkedList<GameBoardCell>(); 
+
+
 	public Actor(ActorType type)
 	{
 		_type = type;
@@ -14,4 +16,9 @@ public class Actor {
 	{
 		return _type;
 	}
+	
+	public java.util.LinkedList<GameBoardCell> getActorPositions() {
+		return actorCells;
+	}
+	
 }
