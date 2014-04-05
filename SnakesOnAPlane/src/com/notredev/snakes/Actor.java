0b0f45469692e09a03c2e155cedfa0fbedcea32b
EJ Layne <ej.layne@playonsports.com>
2014-04-05 16:10:41 -0700

    updated Actor again

--- SnakesOnAPlane/src/com/notredev/snakes/Actor.java
+++ SnakesOnAPlane/src/com/notredev/snakes/Actor.java
@@ -4,12 +4,13 @@ public class Actor {
 	
 	enum ActorType {SNAKE, OBSTACLE, BULLET}
 	ActorType _type;
-	java.util.LinkedList<GameBoardCell> actorCells = new java.util.LinkedList<GameBoardCell>(); 
+	java.util.LinkedList<GameBoardCell> _actorCells = new java.util.LinkedList<GameBoardCell>(); 
 
 
-	public Actor(ActorType type)
+	public Actor(ActorType type, java.util.LinkedList<GameBoardCell> actorCells)
 	{
 		_type = type;
+		_actorCells = actorCells;
 	}
 
 	public ActorType getType()
@@ -17,8 +18,8 @@ public class Actor {
 		return _type;
 	}
 	
-	public java.util.LinkedList<GameBoardCell> getActorPositions() {
-		return actorCells;
+	public java.util.LinkedList<GameBoardCell> getActorCells() {
+		return _actorCells;
 	}
 	
 }
--- SnakesOnAPlane/src/com/notredev/snakes/Obstacle.java
+++ SnakesOnAPlane/src/com/notredev/snakes/Obstacle.java
@@ -2,8 +2,8 @@ package com.notredev.snakes;
 
 public class Obstacle extends Actor {
 	
-	public Obstacle(ActorType type) {
-		super(type);
+	public Obstacle(ActorType type, java.util.LinkedList<GameBoardCell> actorCells) {
+		super(type, actorCells);
 		// TODO Auto-generated constructor stub
 	}
 
